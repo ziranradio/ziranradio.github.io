46aa4e7203b548710059a249d759415397afaabe
ziranradio <yaobik.zjy@gmail.com>
2026-09-12 09:50:51 +0800

    feat: bind tracks to uploaded local audio

--- tracks.js
+++ tracks.js
@@ -3,42 +3,42 @@ window.RADIO_TRACKS = [
     "date": "0613",
     "title": "遇见",
     "artist": "孙燕姿",
-    "audio": "",
+    "audio": "song-0613.mp3",
     "source": "https://y.qq.com/n/ryqq_v2/search?w=%E9%81%87%E8%A7%81%20%E5%AD%99%E7%87%95%E5%A7%BF"
   },
   {
     "date": "0706",
     "title": "像我这样的人",
     "artist": "毛不易",
-    "audio": "",
+    "audio": "song-0706.mp3",
     "source": "https://y.qq.com/n/ryqq_v2/search?w=%E5%83%8F%E6%88%91%E8%BF%99%E6%A0%B7%E7%9A%84%E4%BA%BA%20%E6%AF%9B%E4%B8%8D%E6%98%93"
   },
   {
     "date": "0725",
     "title": "世界赠予我的",
     "artist": "王菲",
-    "audio": "",
+    "audio": "song-0725.mp3",
     "source": "https://y.qq.com/n/ryqq_v2/search?w=%E4%B8%96%E7%95%8C%E8%B5%A0%E4%BA%88%E6%88%91%E7%9A%84%20%E7%8E%8B%E8%8F%B2"
   },
   {
     "date": "0811",
     "title": "山丘",
     "artist": "李宗盛",
-    "audio": "",
+    "audio": "song-0811.mp3",
     "source": "https://y.qq.com/n/ryqq_v2/search?w=%E5%B1%B1%E4%B8%98%20%E6%9D%8E%E5%AE%97%E7%9B%9B"
   },
   {
     "date": "0826",
     "title": "自由飞翔",
     "artist": "凤凰传奇",
-    "audio": "",
+    "audio": "song-0826.mp3",
     "source": "https://y.qq.com/n/ryqq_v2/search?w=%E8%87%AA%E7%94%B1%E9%A3%9E%E7%BF%94%20%E5%87%A4%E5%87%B0%E4%BC%A0%E5%A5%87"
   },
   {
     "date": "0912",
     "title": "我的果汁分你一半",
     "artist": "大张伟",
-    "audio": "",
+    "audio": "song-0912.mp3",
     "source": "https://y.qq.com/n/ryqq_v2/search?w=%E6%88%91%E7%9A%84%E6%9E%9C%E6%B1%81%E5%88%86%E4%BD%A0%E4%B8%80%E5%8D%8A%20%E5%A4%A7%E5%BC%A0%E4%BC%9F"
   }
 ];
